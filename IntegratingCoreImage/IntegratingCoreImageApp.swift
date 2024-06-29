e0625ea1b8cf46587296d13b2f5ad46cd2e74830
Víctor Ávila <victoraavila2000@gmail.com>
2024-06-28 21:06:11 -0300

    Initial Commit

---
+++ IntegratingCoreImage/IntegratingCoreImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegratingCoreImageApp.swift
+//  IntegratingCoreImage
+//
+//  Created by Víctor Ávila on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct IntegratingCoreImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
